1a4245e1af926cf20d44a3b5408c88371fd3b7f4
SHASHANK <151935629+radhikashashi@users.noreply.github.com>
2025-01-03 10:07:49 +0530

    Demo 4.java

--- Demo 4.java
+++ Demo 4.java
@@ -1,6 +1,6 @@
 public class DisplayNumbers {
     public static void main(String[] args) {
-        // Loop from 1 to 100
+        
         for (int i = 1; i <= 100; i++) {
             System.out.println(i);
         }
