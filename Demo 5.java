beb69a4e706570f8081a487507cd6b0ed335ec88
SHASHANK <151935629+radhikashashi@users.noreply.github.com>
2025-01-03 10:06:49 +0530

    Demo 5.java

--- Demo 5.java
+++ Demo 5.java
@@ -9,7 +9,7 @@ class Demo5{
         Scanner scanner = new Scanner(System.in);
         boolean isAuthenticated = false;
 
-        // Use a while loop to authenticate the user
+       
         while (!isAuthenticated) {
             System.out.print("Enter username: ");
             String inputUsername = scanner.nextLine();
@@ -17,7 +17,7 @@ class Demo5{
             System.out.print("Enter password: ");
             String inputPassword = scanner.nextLine();
 
-            // Check if the input matches the valid credentials
+          
             if (inputUsername.equals(validUsername) && inputPassword.equals(validPassword)) {
                 System.out.println("Happy New Year 2025!");
                 isAuthenticated = true;
