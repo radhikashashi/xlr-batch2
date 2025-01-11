1b9046f82c013c3771a864a85efeffd90cdad57f
SHASHANK <151935629+radhikashashi@users.noreply.github.com>
2025-01-11 10:59:13 +0530

    Problem Statement - 05 [Part -2]

--- Employee.java
+++ Employee.java
@@ -1,73 +1,41 @@
-package PS001;
-class Employee {
-    // Instance variables
-    private int id;
-    private String firstName;
-    private String lastName;
-    private String address;
+package PS005;
 
-    // Default constructor
-    public Employee() {
-    }
+public class Employee  {
+	int id;
+	String name;
+	double salary;
+	
+	public Employee() {	}
 
-    // Parameterized constructor
-    public Employee(int id, String firstName, String lastName, String address) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-    }
+	public Employee(int id, String name, double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
 
-    // Getter and Setter methods for instance variables
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    // Method to get full name
-    public String getFullName() {
-        return firstName + " " + lastName;
-    }
-
-    // Main method for testing
-    public static void main(String[] args) {
-        // Using the parameterized constructor
-        Employee emp1 = new Employee(1, "John", "Doe", "123 Main St");
-        System.out.println("Employee 1 Full Name: " + emp1.getFullName());
-
-        // Using the default constructor and setter methods
-        Employee emp2 = new Employee();
-        emp2.setId(2);
-        emp2.setFirstName("Jane");
-        emp2.setLastName("Smith");
-        emp2.setAddress("456 Elm St");
-        System.out.println("Employee 2 Full Name: " + emp2.getFullName());
-    }
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary
+				+ "]";
+	}
 }
+
