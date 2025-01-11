18914d684bf4a579e9fb3404b6a917affc3e20e8
SHASHANK <151935629+radhikashashi@users.noreply.github.com>
2025-01-11 11:27:59 +0530

    Count Of Vowels And Consonants

---
+++ Demo 26.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+ class Demo26 {
+    public static void main(String[] args) {
+       
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        
+        input = input.toLowerCase();
+
+       
+        int vowelCount = 0;
+        int consonantCount = 0;
+
+        
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+
+           
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                vowelCount++;
+            }
+            
+            else if (ch >= 'a' && ch <= 'z') {
+                consonantCount++;
+            }
+        }
+
+        
+        System.out.println("Number of vowels: " + vowelCount);
+        System.out.println("Number of consonants: " + consonantCount);
+
+        
+        scanner.close();
+    }
+}
