7407c7b5418d33644bf2e519f3483e275201da03
SHASHANK <151935629+radhikashashi@users.noreply.github.com>
2025-01-11 11:18:34 +0530

    Reverse String

---
+++ Demo 25.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+ class Demo25 {
+    public static void main(String[] args) {
+       
+        Scanner scanner = new Scanner(System.in);
+
+       
+        System.out.print("Enter a string to reverse: ");
+        String input = scanner.nextLine();
+
+    
+        String reversed = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+
+     
+        System.out.println("Reversed string: " + reversed);
+
+      
+        scanner.close();
+    }
+}
