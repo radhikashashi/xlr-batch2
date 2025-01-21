f0afbb080e0af3ae21e64b53e661bfa707c844c7
SHASHANK <151935629+radhikashashi@users.noreply.github.com>
2025-01-21 09:58:52 +0530

    Hash File

--- Demo 31.java
+++ Demo 31.java
@@ -1,15 +1,23 @@
 import java.util.HashSet;
- class Demo31 {
-public static void main(String[] args) {
+class HashSetDemo1 {
+	void HSetMethod() {
+		
+		HashSet hashSet = new HashSet();
+		hashSet.add(100);
+		hashSet.add("Shashank");
+		hashSet.add(9999.99);
+		System.out.println(hashSet);		
 
-HashSet hashSet = new HashSet();
-hashSet.add(100);
-hashSet.add("SHASHANK");
-hashSet.add(9999.99);
-System.out.println(hashSet);
+		for(Object x: hashSet) {
+			System.out.println(x);
+		}
+	}
+}
 
-for(Object x: hashSet) {
-System.out.println(x);
-}
-}
-}
+
+class Demo31 {
+	public static void main(String[] args) {
+		HashSetDemo1 obj = new HashSetDemo1();
+		obj.HSetMethod();
+		}
+	}
