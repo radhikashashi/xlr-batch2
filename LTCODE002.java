2da7d41837644e46b1f0cb2b88fc6ef0007d9430
SHASHANK <151935629+radhikashashi@users.noreply.github.com>
2025-01-22 10:26:58 +0530

    Reverse Integer

---
+++ LTCODE002.java
@@ -0,0 +1,29 @@
+public class LTCODE002 {
+    public int reverse(int x) {
+        int reversed = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+
+           
+            if (reversed > Integer.MAX_VALUE/10 || (reversed == Integer.MAX_VALUE/10 && digit > 7)) return 0;
+            if (reversed < Integer.MIN_VALUE/10 || (reversed == Integer.MIN_VALUE/10 && digit < -8)) return 0;
+
+            reversed = reversed * 10 + digit;
+        }
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+        LTCODE002 ri = new LTCODE002();
+        
+       
+        System.out.println(ri.reverse(123)); 
+        
+     
+        System.out.println(ri.reverse(-123)); 
+        
+        
+        System.out.println(ri.reverse(120)); 
+    }
+}
