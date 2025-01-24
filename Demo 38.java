485edb20a6d86543ebdf4c81d81282312ebc4ea9
SHASHANK <151935629+radhikashashi@users.noreply.github.com>
2025-01-24 21:16:02 +0530

    To Check Character Lowercase Or Uppercase

---
+++ Demo 38.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+ class Demo38 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a character:");
+        char ch = scanner.next().charAt(0);
+
+        if (Character.isUpperCase(ch)) {
+            System.out.println(ch + " is an uppercase letter.");
+        } else if (Character.isLowerCase(ch)) {
+            System.out.println(ch + " is a lowercase letter.");
+        } else {
+            System.out.println(ch + " is not an alphabetic character.");
+        }
+    }
+}
