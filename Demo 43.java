33719bfdef71d1d7b77f1b23b9c0be05793da842
SHASHANK <151935629+radhikashashi@users.noreply.github.com>
2025-01-27 09:50:58 +0530

    Uppercase Character into Lowercase

---
+++ Demo 43.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Demo43{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Enter the second number:");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Before swapping: num1 = " + num1 + ", num2 = " + num2);
+
+        
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        System.out.println("After swapping: num1 = " + num1 + ", num2 = " + num2);
+    }
+}
