b1f456f5a83fa54f7a8ef44bf648b0fa389b5884
SHASHANK <151935629+radhikashashi@users.noreply.github.com>
2025-01-29 10:10:27 +0530

    Method Overloading

--- Demo054.java
+++ Demo054.java
@@ -1,50 +1,61 @@
+
 class MathOperations {
 
-    public int add(int a , int b) {
+    
+    public int add(int a, int b) {
         return a + b;
     }
 
-    public int add(int a , int b , int c) {
+    
+    public int add(int a, int b, int c) {
         return a + b + c;
     }
 
-    public double add(double a , double b) {
+    
+    public double add(double a, double b) {
         return a + b;
     }
 }
 
+
 class StudentDetails {
 
+    
     public void display(String name) {
-        System.out.println("Student Name: "+ name);
+        System.out.println("Student Name: " + name);
     }
 
+    
     public void display(String name, int age) {
-        System.out.println("Student Name: "+ name);
-        System.out.println("Age: "+ age);
+        System.out.println("Student Name: " + name);
+        System.out.println("Age: " + age);
     }
 
-    public void display(String name, int age , char grade) {
-        System.out.println("Student Name: "+ name);
-        System.out.println("Age: "+ age);
-        System.out.println("Grade: "+ grade);
+    
+    public void display(String name, int age, char grade) {
+        System.out.println("Student Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Grade: " + grade);
     }
 }
 
-public class Demo054{
+
+public class Demo054 {
 
     public static void main(String[] args) {
+        
         MathOperations mathOps = new MathOperations();
-        StudentDetails studDet = new StudentDetails();
+        StudentDetails studentDetails = new StudentDetails();
 
-        System.out.println("Addition Of 2 Integers: "+ mathOps.add(5 , 10));
-        System.out.println("Addiiton Of 3 Integers: "+ mathOps.add(5, 10, 20));
-        System.out.println("Addition of 2 Doubles: "+ mathOps.add(5.56 , 6.66));
+        
+        System.out.println("Addition of two integers: " + mathOps.add(5, 10));
+        System.out.println("Addition of three integers: " + mathOps.add(5, 10, 20));
+        System.out.println("Addition of two double values: " + mathOps.add(5.56, 6.66));
 
-        System.out.println("\n Student Details: ");
-        studDet.display("Shashank");
-        studDet.display("Vignesh" , 20);
-        studDet.display("Pavan" , 10 , 'A');
-
+        
+        System.out.println("\nStudent Details:");
+        studentDetails.display("Shashank");
+        studentDetails.display("Vignesh", 20);
+        studentDetails.display("Pavan", 10, 'A');
     }
 }
